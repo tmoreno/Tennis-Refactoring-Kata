3f037a5d4ae840e2eb1471e86678788bfe241488
tmorenobernal <tmorenobernal@gmail.com>
2022-10-22 17:40:00 +0200

    Early return

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -18,13 +18,14 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (isDraw(pointsPlayer1, pointsPlayer2)) {
+            return drawScore(pointsPlayer1);
+        }
+
         String score = "";
         int tempScore=0;
-        if (isDraw(pointsPlayer1, pointsPlayer2))
-        {
-            score = drawScore(pointsPlayer1);
-        }
-        else if (pointsPlayer1 >=4 || pointsPlayer2 >=4)
+
+        if (pointsPlayer1 >=4 || pointsPlayer2 >=4)
         {
             int minusResult = pointsPlayer1 - pointsPlayer2;
             if (minusResult==1) score ="Advantage player1";
