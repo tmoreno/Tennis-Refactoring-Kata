08f4d29429c83e1aedef0474746d5c7106b4d279
tmorenobernal <tmorenobernal@gmail.com>
2022-10-22 18:22:47 +0200

    Remove unused attributes

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,17 +1,12 @@
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
     
     public String P1res = "";
     public String P2res = "";
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public String getScore(){
